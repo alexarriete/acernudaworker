6ce2a49beacda2403d52f7ce8fd947bc0cc25c1b
Alejandro Cernuda <alejandro@acernuda.com>
2025-10-21 14:13:24 +0200

    profile name

--- src/auth/fetchhandler.js
+++ src/auth/fetchhandler.js
@@ -1,29 +1,32 @@
 import {authMiddleware, isValidIssuer} from "./login"
 import {getLayout} from '../layout'
+import { insertFragmentById } from '../utils/inserter';
 
-let isAuthenticated = false;
+
+
 export async function fetchHandler(request) {
   const url = new URL(request.url);
 
   // Middleware de auth
   const authResponse = await authMiddleware(request, url);
-  if (authResponse) return authResponse;
-  let layout = await getLayout(request);
+  if (authResponse) return authResponse;  
   // Middleware de rutas protegidas
   const protectedResponse = await protectedMiddleware(request, url);
-  if (protectedResponse) return protectedResponse;
-    
-  // Ruta pública
-  if(isAuthenticated){
-    layout = layout.replace("auth/login", "perfil");
-    layout = layout.replace('"login"', "nombre de usuario");
-  } 
+  if (protectedResponse ===1) return new Response(null, {status: 302, headers: { "Location": "/auth/login" }});
   
-  if (url.pathname === "/" || url.pathname === "/index.html") {
-    return new Response(layout, { headers: { "Content-Type": "text/html" } });
+  let layout = protectedResponse !== 0 ? await getLayout(request): "";
+  //Require layout and is uthenticated
+  if(protectedResponse== 2){          
+      layout = layout.replace("auth/login", "perfil");
+      layout = layout.replace('"login"', "nombre de usuario");    
   }
-
-  return new Response("Not Found", { status: 404 });
+  
+  // Ruta pública
+  if(layout.indexOf('layoutcontent')>0){
+     layout =  await insertFragmentById(layout, 'layoutcontent', "not found")
+     return new Response(layout, { headers: { "Content-Type": "text/html", status:404 } });
+  }
+  return new Response(layout, { headers: { "Content-Type": "text/html" } });  
 }
 
 function parseJwt(token) {
@@ -37,35 +40,40 @@ function parseJwt(token) {
 }
 
 async function protectedMiddleware(request, url) {
-  if (url.pathname.startsWith("/auth")) {
-    const cookie = request.headers.get("Cookie") || "";
-    const match = cookie.match(/id_token=([^;]+)/);
-    if (!match) {
-      return new Response(null, {
-        status: 302,
-        headers: { "Location": "/auth/login" }
-      });
+  if (requiresLayout(url)) {
+     return validToken(request)? 1:2;
+    } 
+    
+  return 0;
+}
+
+function requiresLayout( url){
+  const authPaths = ["nota"];  
+
+  const pathSegments = url.pathname
+                          .split('/')
+                          .filter(segment => segment.length > 0);
+
+  if (pathSegments.length > 0) {
+    const primerSegmento = pathSegments[0];     
+    if (authPaths.includes(primerSegmento)) {
+      return false;
     }
+  }
+  return true;  
+}
+
+function validToken(request){
+  const cookie = request.headers.get("Cookie") || "";
+    const match = cookie.match(/id_token=([^;]+)/);
+    if (!match) return false;
 
     const token = match[1];
     const payload = parseJwt(token);
-    if (!payload || !isValidIssuer(payload.iss) ) {
-            // Token inválido      
-      return new Response(null, {
-        status: 302,
-        headers: { "Location": "/auth/login" }
-      });
-    }
+    if (!payload || !isValidIssuer(payload.iss) ) return false;            
+    
 
     // Verificar expiración
     const now = Math.floor(Date.now() / 1000);
-    if (payload.exp && payload.exp < now) {
-      return new Response(null, {
-        status: 302,
-        headers: { "Location": "/auth/login" }
-      });
-    }    
-  }
-  isAuthenticated = true;
-  return null;
+    if (payload.exp && payload.exp < now && url.pathname.startsWith("/auth")) return false;
 }
